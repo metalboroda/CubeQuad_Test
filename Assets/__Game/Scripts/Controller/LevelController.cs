b66f28dddbf198611e0ae7228f0c09edadb5d9cd
dageron93 <vladb93games@gmail.com>
2023-12-24 21:11:40 +0200

    Added random level logic

--- Assets/__Game/Scripts/Controller/LevelController.cs
+++ Assets/__Game/Scripts/Controller/LevelController.cs
@@ -8,19 +8,32 @@ namespace CubeQuad
 {
   public class LevelController : MonoBehaviour
   {
+    [SerializeField] private int _levelAmount;
+
+    [Header("")]
     [SerializeField] private PlayerPrefsController _playerPrefsController;
 
     private string _address;
     private int _levelIndex;
+    private int _randomLevelIndex;
     private GameObject _spawnedLevel;
 
     [Inject] private DiContainer _spawnContainer;
 
     private async void Start()
     {
-      _levelIndex = _playerPrefsController.GetLevelIndex();
+      if (_playerPrefsController.GetIsRandomLevel() == 0)
+      {
+        _levelIndex = _playerPrefsController.GetLevelIndex();
 
-      SetAddressIndex(_levelIndex);
+        SetAddressIndex(_levelIndex);
+      }
+      else
+      {
+        _randomLevelIndex = _playerPrefsController.GetRandomLevelIndex();
+
+        SetAddressIndex(_randomLevelIndex);
+      }
 
       await LoadLevel(_address);
     }
@@ -32,12 +45,45 @@ namespace CubeQuad
 
     public void LoadNextLevel()
     {
-      _levelIndex++;
-      _playerPrefsController.SaveLevelIndex(_levelIndex);
+      if (_playerPrefsController.GetIsRandomLevel() == 0)
+      {
+        _levelIndex++;
+        _playerPrefsController.SaveLevelIndex(_levelIndex);
 
-      SetAddressIndex(_levelIndex);
+        if (_levelIndex > _levelAmount)
+        {
+          LoadRandomLevel();
+        }
+        else
+        {
+          SetAddressIndex(_levelIndex);
+
+          SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+      }
+      else
+      {
+        int randLevel = Random.Range(0, _levelAmount + 1);
+
+        _playerPrefsController.SaveRandomLevelIndex(randLevel);
+
+        SetAddressIndex(randLevel);
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+      }
+    }
+
+    private void LoadRandomLevel()
+    {
+      int randLevel = Random.Range(1, _levelAmount + 1);
+
+      _playerPrefsController.SaveLevelIndex(randLevel);
+
+      SetAddressIndex(randLevel);
 
       SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+      _playerPrefsController.SetIsRandomLevel(1);
     }
 
     public async Task<GameObject> LoadLevel(string address)
@@ -48,6 +94,7 @@ namespace CubeQuad
       if (loadOperation.Status == UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationStatus.Succeeded)
       {
         GameObject prefab = loadOperation.Result;
+
         _spawnedLevel = _spawnContainer.InstantiatePrefab(prefab);
 
         return _spawnedLevel;
--- Assets/__Game/Scripts/Controller/PlayerPrefsController.cs
+++ Assets/__Game/Scripts/Controller/PlayerPrefsController.cs
@@ -14,6 +14,26 @@ namespace CubeQuad
     {
       PlayerPrefs.SetInt("LevelIndex", index);
     }
+
+    public int GetRandomLevelIndex()
+    {
+      return PlayerPrefs.GetInt("RandomLevelIndex");
+    }
+
+    public void SaveRandomLevelIndex(int index)
+    {
+      PlayerPrefs.SetInt("RandomLevelIndex", index);
+    }
+
+    public int GetIsRandomLevel()
+    {
+      return PlayerPrefs.GetInt("IsRandomLevel");
+    }
+
+    public void SetIsRandomLevel(int value)
+    {
+      PlayerPrefs.SetInt("IsRandomLevel", value);
+    }
     #endregion
   }
 }
